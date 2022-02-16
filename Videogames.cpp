996e4a9d98fdf2b620ab68a5f5b7543aa3b1f1e7
MohammedTALAL1 <>
2022-02-16 23:24:52 +0000

    Project 2 in Progress

--- Videogames.cpp
+++ Videogames.cpp
@@ -48,16 +48,31 @@ int validInt(int num) {
 }
 
 void printGames(ostream &out, videogametype game){
-  out << game.name << ";";
-  out << game.numSales << ";";
-  out << game.platform << ";";
-  out << game.releaseDate << ";";
-  out << game.developer << ";";
-  out << game.publisher << endl;
-  
+    out << game.name << ";";
+    out << game.numSales << ";";
+    out << game.platform << ";";
+    out << game.releaseDate << ";";
+    out << game.developer << ";";
+    out << game.publisher << endl;
 }
 
-void addGame(videogametype games[]) {
+void removeGame(videogametype game[]) {
+  int index = 0;
+  cout << "Enter the index you want to remove: ";
+  cin >> index;
+  cin.ignore();
+  index = validInt(index);
+  for (int counter = 0; counter < 100; counter++) {
+    game[index].name[counter] = '\0';
+    game[index].platform[counter] = '\0';
+    game[index].releaseDate[counter] = '\0';
+    game[index].developer[counter] = '\0';
+    game[index].publisher[counter] = '\0';
+  }
+  game[index].numSales = 0;
+}
+
+void addGame(videogametype games[], int &num) {
   char temp[100];
   for (int index = 0; index < 100; index++) {
     temp[index] = '\0';
@@ -66,23 +81,20 @@ void addGame(videogametype games[]) {
   for (int index = 0; index < 100; index++) {
     if (strcmp(games[index].name,temp) == 0) {
       cout << "Enter the game's name: ";
-      cin.ignore();
       cin.getline(games[index].name, 100);
       cout << endl << "Enter the number of sales: ";
       cin >> games[index].numSales;
       games[index].numSales = validDouble(games[index].numSales);
-      cout << "Enter the platform: ";
       cin.ignore();
+      cout << "Enter the platform: ";
       cin.getline(games[index].platform, 100);
       cout << "Enter the game's release Date (format: Month day, year): ";
-      cin.ignore();
       cin.getline(games[index].releaseDate, 100);
       cout << "Enter the game's developer: ";
-      cin.ignore();
       cin.getline(games[index].developer, 100);
       cout << "Enter the game's publisher: ";
-      cin.ignore();
       cin.getline(games[index].publisher, 100);
+      num++;
       break;
     }
   }
@@ -159,9 +171,19 @@ void searchGameByName(int num, videogametype games[]) {
 }
 
 void printGamesToScreen(int num, videogametype games[]) {
+  char selecInput[100];
+  for (int index = 0; index < 100; index++) {
+    selecInput[index] = '\0';
+  }
+
   for (int index = 0; index < num; index++) {
+    if(strcmp(games[index].name,selecInput) != 0) {
         cout << "Index " << index << ": ";
         printGames(cout, games[index]);
+    }
+    else {
+      cout << endl;
+    }
   }
 }
 
@@ -186,7 +208,12 @@ videogametype readGame(ifstream &inFile){
     return game;
 }
 
-
+void printDataToFile(videogametype games[], int num) {
+  ofstream outFile("videogames.txt");
+    for (int index = 0; index < num; index++) {
+        printGames(outFile, games[index]);
+    }
+}
 
 int readGameData(videogametype example[]) {
   ifstream gameFile("videogamesOriginal.txt");
@@ -198,17 +225,75 @@ int readGameData(videogametype example[]) {
   return numGames;
 }
 
+void displayMenu(videogametype games[], int num){
+  int option = 0;
+  int perameter = 0;
+  while (option != 7) {
+    cout << endl << "1: Display the game list" << endl;
+    cout << "2: Remove a game from the list" << endl;
+    cout << "3: Add a game to the list" << endl;
+    cout << "4: Search game by perameter" << endl;
+    cout << "5: Reload original data" << endl;
+    cout << "6: Save current data" << endl;
+    cout << "7: Quit" << endl;
+    cout << "Enter an option: ";
+    cin >> option;
+    cin.ignore();
+    option = validInt(option);
+  
+    if(option == 1) {
+      printGamesToScreen(num, games);
+    }
+    else if (option == 2) {
+      removeGame(games);
+    }
+    else if (option == 3) {
+     addGame(games, num);
+    }
+    else if (option == 4) {
+      cout << "1) By Name" << endl;
+      cout << "2) By platform" << endl;
+      cout << "3) By developer" << endl;
+      cout << "4) By publisher" << endl;
+      cout << "Enter perameter: ";
+      cin >> perameter;
+      cin.ignore();
+      perameter = validInt(perameter);
+      if (perameter == 1) {
+        searchGameByName(num, games);
+      }
+      else if (perameter == 2) {
+        searchGameByPlatform(num, games);
+      }
+      else if (perameter == 3) {
+        searchGameByDeveloper(num, games);
+      }
+      else if (perameter == 4) {
+        searchGameByPublisher(num, games);
+      }
+      else {
+        cout << "Invalid Option!!!";
+      }
+    }
+    else if (option == 5) {
+      readGameData(games);
+    }
+    else if (option == 6) {
+      printDataToFile(games, num);
+    }
+    else if (option == 7) {
+      break;
+    }
+    else {
+      cout << "Invalid Option!!!";
+    }
+  }
+}
 
 
 
 int main() {
   videogametype games[100];
   int num = readGameData(games);
-  printGamesToScreen(num, games);
-  addGame(games);
-  printGamesToScreen(num, games);
-  // searchGameByName(num, games);
-  // searchGameByDeveloper(num, games);
-  // searchGameByPlatform(num, games);
-  // searchGameByPublisher(num, games);
+  displayMenu(games, num);
 }
